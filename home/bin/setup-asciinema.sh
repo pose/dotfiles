7d9b201b916f6a9824cfb36c14044d80b95fa47c
Alberto Pose <albertopose@gmail.com>
2024-07-10 15:33:24 +0100

    Adding asciinema and tmux-resurrect.

---
+++ home/bin/setup-asciinema.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+python3 -m pip install asciinema
+
+
---
+++ home/bin/setup-tmux-resurrect.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+git clone https://github.com/tmux-plugins/tmux-resurrect ~/.local/lib/tmux-resurrect
